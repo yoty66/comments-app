f20660e8016fe5256ab5d351be5f40de48773c79
yotam <yotam@qlarium.com>
2022-03-14 15:13:54 +0200

    init react app + delete irrelevant files\packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+const IMAGE_URL='https://global-uploads.webflow.com/5d53975001f72a80a5fdf1f2/5f0b178d3ebbaecd6db65217_Steps%20Logo.svg'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={IMAGE_URL} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
