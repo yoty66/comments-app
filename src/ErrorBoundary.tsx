55986f9e5a3eab02e11252adb71350878f895619
yotam <yotam@qlarium.com>
2022-03-15 18:17:14 +0200

    ErrorBoundary.tsx add closing button

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,9 +1,9 @@
 import React, {createContext, useState} from 'react';
-import Button from '@mui/material/Button';
 import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
 
 function ErrorDialog(props) {
@@ -16,6 +16,18 @@ function ErrorDialog(props) {
 
             <DialogTitle  >
                 Error
+                <IconButton
+                    aria-label="close"
+                    onClick={onClose}
+                    sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                    }}
+                >
+                    <CloseIcon />
+                </IconButton>
             </DialogTitle>
             <DialogContent dividers>
                 <Typography gutterBottom>
